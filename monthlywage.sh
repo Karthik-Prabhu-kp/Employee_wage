33391c18fc67519509f6227da6490180543d0c96
Karthik-Prabhu-kp <kpwat66@gmail.com>
2020-08-31 07:53:09 +0530

    commit local change

---
+++ monthlywage.sh
@@ -0,0 +1,27 @@
+#! /bin/bash -x
+isPresent=1
+ispartTime=2
+EmployeeCheck=$(( RANDOM%3 ))
+wageperHour=20
+working_days=20
+	case $EmployeeCheck in
+	$isPresent)
+
+	   echo "Employee is present $isPresent"
+         workhrs=8
+    		dailywage=$((wageperHour*workhrs))
+        echo "$dailywage"	
+	;;
+	$ispartTime)
+
+      echo "Employee is par-time"
+         workhrs=4
+         dailywage=$((wageperHour*workhrs))
+        echo "$dailywage"
+		;;
+		*)
+		echo "Employee is absent"
+	esac
+Totalwage=$((working_days*dailywage))
+        echo "$Totalwage"
+
